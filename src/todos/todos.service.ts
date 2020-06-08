92eedac4188f3387c34ce700f49746e8e4f05f9b
Peranut W <peranut.w@ku.th>
2020-06-08 15:11:39 +0700

    Remove unused import

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpException } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { Todo } from './todos.interface'
 import { AddTodoDto } from './dto/add-todo.dto'
 import { UpdateTodoDto } from './dto/update-todo.dto'
